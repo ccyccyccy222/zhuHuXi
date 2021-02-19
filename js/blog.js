9eb38f153763109e0c2f07788bc75a75f445e93f
ccyccyccy222 <1092098326@qq.com>
2021-02-19 16:17:36 +0800

    完成了新建帖子的功能

--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,44 @@
-urlinfo = window.location.href;  //获取当前页面的url
-console.log(urlinfo)
-len = urlinfo.split("?");//获取url的长度
-newsidinfo = len[1];//取出参数字符串 这里会获得类似“id=1”这样的字符串
-console.log(newsidinfo);
-// newsids = newsidinfo.split("&");//对获得的参数字符串按照“=”进行分割
-// parameter1 = newsids[0];  //得到参数值 os：标识安卓还是ios，1=ios、2=安卓
-// os = parameter1.split("=")[1];
-// parameter2 = newsids[1];   //1=普通屏幕、2=iPhone X、3=iPhone XR、4=iPhoneX Max
-// dim = parameter2.split("=")[1];
-// parameter3 = newsids[2];  //version：广告页版本，1=第一种版本、2=第二种版本，每个版本显示的图片都是不一样的
-// version = parameter3.split("=")[1];
-// console.log("获取到的参数值是" + os, dim, version);
+// urlinfo = window.location.href;  //获取当前页面的url
+// console.log(urlinfo)
+// len = urlinfo.split("?");//获取url的长度
+// newsidinfo = len[1];//取出参数字符串 这里会获得类似“id=1”这样的字符串
+// console.log(newsidinfo);
+
+
+// submit=()=>{
+
+function submit(){
+    let title=document.getElementById("addTitle").value;
+    let content=document.getElementById("addContent").value;
+
+
+    const sourceNode = document.querySelectorAll(".col-md-6"); // 获得被克隆的节点对象
+    //前面有2个.col-md-6类，所以这一个的下标为2
+    const clonedNode = sourceNode[5].cloneNode(true); // 克隆节点
+
+    console.log("clonedNode.getElementsByTagName(\"h3\").[0]innerText before");
+    console.log(clonedNode.getElementsByTagName("h3")[0].innerText);
+    clonedNode.getElementsByTagName("h3")[0].innerText=title;
+    // if(image!=null){
+    //     console.log("In here");
+    //     console.log(image);
+    //     console.log("clonedNode.getElementsByTagName(\"img\")[0]");
+    //     console.log(clonedNode.getElementsByTagName("img")[0]);
+    //     console.log("clonedNode.getElementsByTagName(\"img\")[0].src");
+    //     console.log(clonedNode.getElementsByTagName("img")[0].src);
+    //     //用图片选择器进行了选择
+    //     clonedNode.getElementsByTagName("img")[0].src=image;
+    // }
+    alert("添加成功！")
+    //清空输入框
+    document.getElementById("addTitle").value="";
+    document.getElementById("addContent").value='';
+    //模态框消失
+    $('#myModal').modal("hide");
+
+    sourceNode[3].parentNode.appendChild(clonedNode); // 在父节点插入克隆的节点
+    console.log("sourceNode[3].parentNode");
+    console.log(sourceNode[3].parentNode);
+
+}
+
